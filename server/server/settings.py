12a659acf551178e7029715a9cd4f59eb452ba8f
David Espinal <despinal0425@gmail.com>
2022-01-15 18:28:13 -0500

    change DEBUG false to True

--- server/server/settings.py
+++ server/server/settings.py
@@ -33,11 +33,9 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://todolist-django-assess-app.herokuapp.com/", 
-"localhost", 
-"127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
