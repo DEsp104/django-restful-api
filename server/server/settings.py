092702f567e9f618c579e9b7eed3792ec44741fc
David Espinal <despinal0425@gmail.com>
2022-01-15 16:33:45 -0500

    added local host

--- server/server/settings.py
+++ server/server/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://todolist-django-assess-app.herokuapp.com/", 
+"localhost", 
+"127.0.0.1"]
 
 
 # Application definition
