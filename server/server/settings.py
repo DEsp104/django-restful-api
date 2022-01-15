db03b26862f3c5360eae68d513d05d54f0523689
David Espinal <despinal0425@gmail.com>
2022-01-14 22:15:51 -0500

    added environ

--- server/server/settings.py
+++ server/server/settings.py
@@ -13,23 +13,29 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR_TWO = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
+
+# read th .env file
+environ.Env.read_env(env_file=str(BASE_DIR) + '/.env')   
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
