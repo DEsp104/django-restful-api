effeb745e32babc5736590af9fe8fad02ed24c61
David Espinal <despinal0425@gmail.com>
2022-01-14 21:42:57 -0500

    deploy

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--pse)o64y6sc2&^s-q6wgqosw@c&a7a6_rw96!cy*zcyd_&z5z'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
      'https://localhost:3000'
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#Activate Django-Heroku.
+django_heroku.settings(locals())
