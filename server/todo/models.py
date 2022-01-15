fa16db513af6f91926a3815b22d9759b025c7060
David Espinal <despinal0425@gmail.com>
2022-01-14 21:04:39 -0500

    added the model to the django

---
+++ server/todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
